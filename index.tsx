b25ccceba9de2b3bbd3e7f7ad25559b1056b917c
asinnn70 <66061691+asinnn70@users.noreply.github.com>
2026-02-16 16:12:53 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // <--- BARIS INI WAJIB ADA UNTUK MEMANGGIL DESAIN
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
